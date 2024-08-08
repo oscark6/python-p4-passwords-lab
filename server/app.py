1eb1ccfaceaf9e02e654ef8cdd3808739621e7fa
oscark6 <oscar.kimani1@student.moringaschool.com>
2024-08-08 23:22:37 +0300

    done

--- server/app.py
+++ server/app.py
@@ -28,16 +28,46 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+
+        if session.get('user_id'):
+            
+            user = User.query.filter(User.id == session['user_id']).first()
+            
+            return user.to_dict(), 200
+
+        return {}, 204
+    
 
 class Login(Resource):
-    pass
+    def post(self):
+
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': '401 Unauthorized'}, 401
+    
 
 class Logout(Resource):
-    pass
+   def delete(self):
+
+        session['user_id'] = None
+
+        return {}, 204
+   
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
-if __name__ == '__main__':
+if __name__ == '_main_':
     app.run(port=5555, debug=True)
